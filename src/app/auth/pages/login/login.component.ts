5a9240d8939ed18b4e20dd3971772352b61ea7c1
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-08-13 21:12:05 -0500

    creacion de usuarios y almacenamiento en firestore

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -4,7 +4,10 @@ import firebase from 'firebase/compat/app';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { StorageService } from '../../../core/storage/storage.service';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
 import { User } from '../../../models/user';
 
 @Component({
@@ -33,17 +36,25 @@ export class LoginComponent implements OnInit {
       .signInWithPopup(new firebase.auth.GoogleAuthProvider())
       .then((value) => {
         const userCollection = this.afs.collection<User>('users');
-
-        this.afs.collectionGroup('users')
-        const userLogin: User = {
-          uid: value.user?.uid?? '1',
-          email: value.user?.email ?? 'noEmail@email.com',
-          username: value.user?.displayName ?? 'anonymous',
-          photo: value.user?.photoURL ?? 'https://icon-library.com/images/icon-person/icon-person-4.jpg'
-        }
-        userCollection.add(userLogin);
-
-        this.storageService.store('user',btoa( JSON.stringify(value.user)));
+        this.afs
+          .collection<User>('users', (ref) =>
+            ref.where('uid', '==', value.user?.uid ?? 1)
+          )
+          .valueChanges()
+          .subscribe((users) => {
+            if (!(users.length > 0)) {
+              const userLogin: User = {
+                uid: value.user?.uid ?? '1',
+                email: value.user?.email ?? 'noEmail@email.com',
+                username: value.user?.displayName ?? 'anonymous',
+                photo:
+                  value.user?.photoURL ??
+                  'https://icon-library.com/images/icon-person/icon-person-4.jpg',
+              };
+              userCollection.add(userLogin);
+            }
+          });
+        this.storageService.store('user', btoa(JSON.stringify(value.user)));
         this.router.navigate(['/auth/started']);
       });
   }
